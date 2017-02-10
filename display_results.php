6aa27281ef5677c91540c1b6e6855423ed234d3c
mehvishhashmi <mh449@njit.edu>
2017-02-10 12:53:20 -0500

    adding display_results

---
+++ display_results.php
@@ -0,0 +1,53 @@
+<?php
+
+ // get the data from the form
+     $investment = filter_input(INPUT_POST, 'investment', FILTER_VALIDATE_FLOAT);
+     $interest_rate = filter_input(INPUT_POST, 'interest_rate',FILTER_VALIDATE_FLOAT);
+     $years = filter_input(INPUT_POST, 'years', FILTER_VALIDATE_INT);
+
+				        
+// if an error message exists, go to the index page
+	if ($error_message != '') {
+	       include('index.php');
+	       exit(); }
+
+// calculate the future value
+	$future_value = $investment;
+	for ($i = 1; $i <= $years; $i++) {
+	     $future_value = 
+	          $future_value + ($future_value * $interest_rate * .01); 
+	}
+
+// apply currency and percent formatting
+	$investment_f = '$'.number_format($investment,2);
+	$yearly_rate_f =$interest_rate.'%';
+	$future_value_f ='$'.number_format($future_value,2);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Future Value Calculator</title>
+        <link rel="stylesheet" type="text/css" href="main.css">
+	</head>
+	<body>
+	    <main>
+	    <h1>Future Value Calculator</h1>
+
+	    <label>Investment Amount:</label>
+	    <span><?php echo $investment_f; ?></span><br>
+
+	    <label>Yearly Interest Rate:</label>
+            <span><?php echo $yearly_rate_f; ?></span><br>
+
+            <label>Number of Years:</label>
+	    <span><?php echo $years; ?></span><br>
+
+            <label>Future Value:</label>
+	    <span><?php echo $future_value_f; ?></span><br>
+	 </main>
+</body>
+</html>
